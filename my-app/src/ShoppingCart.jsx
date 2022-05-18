57ba692671d3384747d62fc331fb1c67ef877d39
mohammed-essam <mohammedesaam72@gmail.com>
2022-05-18 13:59:14 +0200

    #

--- my-app/src/ShoppingCart.jsx
+++ my-app/src/ShoppingCart.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Product from './product'
+
 class ShoppingCart extends Component {
     state = { 
         products: [
@@ -11,21 +12,49 @@ class ShoppingCart extends Component {
      handleDelete =(product) => {
         //Clone
         //Edit
-        const newproducts= this.state.products.filter(p=>p.id !==product.id)
+        const products= this.state.products.filter(p=>p.id !==product.id)
         //Set state
-        this.setState({products:newproducts})
+        this.setState({products})
      }
+     handleReset =()=>{
+         //clone
+        let products= [...this.state.products]
+         //Edit
+        products = products.map( p => {
+            p.count=0;
+             return p
+            })
+         // Set state
+         this.setState({products})
+     }
+     handleIncrement = (product) => {
+         // clone
+            const products = [...this.state.products]
+            //deep clone object to maintain increment function
+            const index = products.indexOf(product)
+            products[index]={...products[index]}
+         // Edit
+         products[index].count++;
+         // Set state
+         this.setState({products})
+        }
+
     render() { 
         return (
             <React.Fragment>
                 <h1 className='badge bg-info  m-2'>shoppingCart</h1>
+                <br />
+                <button onClick={this.handleReset} className='fa-solid fa-zero btn btn-warning btn-sm m-2'>Reset</button>
                 {this.state.products.map(product => (
-                <Product key={product.id} onDelete={this.handleDelete} product={product}>
-                    <h4>{product.id}</h4>
+                <Product key={product.id} 
+                    onDelete={this.handleDelete}
+                    onIncrement={this.handleIncrement}
+                product={product}>
+                <h4>{product.id}</h4>
                 </Product>
                 ))}
-            </React.Fragment>);
-            
+            </React.Fragment>
+        );      
     }
 }
  
--- my-app/src/product.jsx
+++ my-app/src/product.jsx
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
 
 class Product extends Component {
-    state = { 
-        name: this.props.product.name,
-        count: this.props.product.count
-     }; 
+    // to have single source of truth not use state || this.state
+    // state = { 
+    //     name: this.props.product.name,
+    //     count: this.props.product.count
+    //  }; 
      
-      classes(){return this.state.count=== 0 ? 'badge bg-warning m-2': 'badge bg-primary m-2'}
-     increamentFun =()=> {this.setState({count: this.state.count +1});}
-     decreamentFun =()=> {this.setState({count: this.state.count -1});}
-     zeroFun =()=> {this.setState({count: this.state.count= 0});}
+            classes(){return this.props.product.count=== 0 ? 'badge bg-warning m-2': 'badge bg-primary m-2'}
+            // decreamentFun =()=> {this.setState({count: this.props.product.count -1});}
+            // zeroFun =()=> {this.setState({count: this.props.products.count= 0});}
+    
     render() { 
-            return (
-        <div className='row'>
-            {/* <id>{this.props.children}</id> */}
-            <div className="col-1">
-            <span className='m-3' >{this.props.product.id}</span>
+        return (
+            <div className='row'>
+                    {/* <id>{this.props.children}</id> */}
+                <div className="col-1">
+                    <span className='m-3' >{this.props.product.id}</span>
+                </div>
+                <div className="col-2">
+                    {this.props.product.name.length === 0 && <h4>No Names</h4> }
+                    <span>{this.props.product.name}</span>
+                </div>
+                <div className="col-1">
+                    <span className= {this.classes()}> {this.props.product.count}</span>
+                </div>
+                <div className="col-4">
+                    <button onClick={()=> {this.props.onIncrement(this.props.product)}} className= "fa-solid fa-plus btn btn-light btn-sm m-1"></button>
+                    <button onClick={this.decreamentFun} className= "fa-solid fa-minus btn btn-light btn-sm m-1"></button>
+                    <i onClick={()=>{this.props.onDelete(this.props.product)}} className="fa-solid fa-trash btn btn-light btn-sm m-1"></i>
+                </div>
             </div>
-            <div className="col-2">
-            {this.state.name.length === 0 && <h4>No Names</h4> }
-            <span>{this.state.name}</span>
-            </div>
-            <div className="col-1">
-            <span className= {this.classes()}> {this.state.count}</span>
-            </div>
-            <div className="col-4">
-            <button onClick={this.increamentFun} className= "fa-solid fa-plus btn btn-light btn-sm m-1"></button>
-            <button onClick={this.decreamentFun} className= "fa-solid fa-minus btn btn-light btn-sm m-1"></button>
-            <button onClick={this.zeroFun} className='fa-solid fa-zero btn btn-light btn-sm m-1'>0</button>
-            <i onClick={()=>{this.props.onDelete(this.props.product)}} className="fa-solid fa-trash btn btn-light btn-sm m-1"></i>
-            </div>
-        </div>);
+        );
     }
 }
  
