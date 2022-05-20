77ff08ccebc11621896af53099224876f0072f26
mohammed-essam <mohammedesaam72@gmail.com>
2022-05-20 15:34:53 +0200

    move the state from shoppingCart-file to app-file to make lifting state up for navbar can read and access the state

--- my-app/src/component/ShoppingCart.jsx
+++ my-app/src/component/ShoppingCart.jsx
@@ -1,72 +1,26 @@
 import React, { Component } from 'react';
-import Product from './product'
-// import NavBar from './navbar';
+
 
 
 class ShoppingCart extends Component {
-    state = { 
-        products: [
-            {id:1, name:"burger", count:2},
-            {id:2, name:"fries", count:1},
-            {id:3, name:"cola", count:3}
-        ]
-     }
-    handleDelete =(product) => {
-        //Clone
-        let products= [...this.state.products]
-        //Edit
-        products= this.state.products.filter(p=>p.id !==product.id)
-        //Set state
-        this.setState({products})
-     }
-    handleReset =()=>{
-         //clone
-        let products= [...this.state.products]
-         //Edit
-        products = products.map( p => {
-            p.count=0;
-             return p
-            })
-         // Set state
-         this.setState({products})
-     }
-    handleIncrement = (product) => {
-        // clone
-        const products = [...this.state.products]
-        //deep clone object to maintain increment function
-        const index = products.indexOf(product)
-        products[index]={...products[index]}
-        // Edit
-        products[index].count++;
-        // Set State
-        this.setState({products})
-        }
-    handleDecrement = (product)=>{
-        // clone
-        const products = [...this.state.products]
-        //deep clone object to maintain increment function
-        const index = products.indexOf(product)
-        products[index]={...products[index]}
-        // Edit
-        products[index].count--;
-        // Set State
-        this.setState({products})
-        }
+    {/* move  state to app file to make lifting state up for navbar can read and access the state  */}
     render() { 
         return (
             <React.Fragment>
                 <h1 className='badge bg-info  m-2'>shoppingCart</h1>
                 <br />
-                <button onClick={this.handleReset} className='fa-solid fa-zero btn btn-warning btn-sm m-2'>Reset</button>
-                {this.state.products.map(product => (
+                <button onClick={this.props.onReset} className='fa-solid fa-zero btn btn-warning btn-sm m-2'>Reset</button>
+                {/* move this function to app file to make lifting state up for navbar can read and access state  */}
+                {/* {this.props.products.map(product => (
                 <Product key={product.id} 
-                    onDelete={this.handleDelete}
-                    onIncrement={this.handleIncrement}
-                    onDecrement={this.handleDecrement}
-                product={product}>
+                
+                    onDelete={()=>{this.props.handleDelete(this.props.product)}}
+                    onIncrement={()=>{this.props.handleIncrement(this.props.product)}}
+                    onDecrement={()=>{this.props.handleDecrement(this.props.product)}} 
+                    product={product}>
                 <h4>{product.id}</h4>
                 </Product>
-                ))}
+                ))} */}
             </React.Fragment>
         );      
     }
--- my-app/src/component/app.jsx
+++ my-app/src/component/app.jsx
@@ -1,17 +1,74 @@
 import React, { Component } from 'react';
 import NavBar from './navbar';
 import ShoppingCart from './ShoppingCart';
+import Product from './product';
 
 
 
 class App extends Component {
-    state = {  } 
+    state = { 
+        products: [
+            {id:1, name:"burger", count:1},
+            {id:2, name:"fries", count:4},
+            {id:3, name:"cola", count:3}
+        ]
+     }
+    handleDelete =(product) => {
+        //Clone
+        let products= [...this.state.products]
+        //Edit
+        products= this.state.products.filter(p=>p.id !==product.id)
+        //Set state
+        this.setState({products})
+     }
+    handleReset =()=>{
+         //clone
+        let products= [...this.state.products]
+         //Edit
+        products = products.map( p => {
+            p.count=0;
+             return p
+            })
+         // Set state
+         this.setState({products})
+     }
+    handleIncrement = (product) => {
+        // clone
+        const products = [...this.state.products]
+        //deep clone object to maintain increment function
+        const index = products.indexOf(product)
+        products[index]={...products[index]}
+        // Edit
+        products[index].count++;
+        // Set State
+        this.setState({products})
+        }
+    handleDecrement = (product)=>{
+        // clone
+        const products = [...this.state.products]
+        //deep clone object to maintain increment function
+        const index = products.indexOf(product)
+        products[index]={...products[index]}
+        // Edit
+        products[index].count--;
+        // Set State
+        this.setState({products})
+        } 
     render() { 
         return (
             <React.Fragment>
                 <NavBar/>
                 <main className="container">
-                    <ShoppingCart/>
+                    <ShoppingCart onReset={this.handleReset}/> 
+                    {this.state.products.map(product => (
+                        <Product key={product.id} 
+                        onDelete={this.handleDelete}
+                        onIncrement={this.handleIncrement}
+                        onDecrement={this.handleDecrement} 
+                        product={product}>
+                        <h4>{product.id}</h4>
+                        </Product>
+                    ))}
                 </main>
             </React.Fragment>
         );
